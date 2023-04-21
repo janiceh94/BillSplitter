cd52ded4994a28bea935549bd8025375cac9b3d0
janiceh94 <haesung94@gmail.com>
2023-04-20 17:09:10 -0700

    Stylized total component

--- src/components/total/Total.js
+++ src/components/total/Total.js
@@ -3,16 +3,19 @@ import styles from "./Total.module.css";
 export default function Total(){
     return (
         <div className={styles.totalContainer}>
-            <form>
-                <label htmlFor="tipAmount">Tip Amount <span>/ person</span></label>
-                <p>$0.00</p>
-                <label htmlFor="total">
-                    Total <span>/ person</span>
-                </label>
-                <div className={styles.resetBtn}>
-                <button className={styles.btn} >RESET</button>
+            <div className={styles.calculation}>
+                <div className={styles.grid}>
+                    <p className={styles.title}>Tip Amount<br/>/ person</p>
+                    <p className={styles.amount}>$0.00</p>
                 </div>
-            </form>
+                <div className={styles.grid}>
+                    <p className={styles.title}>Total<br/>/ person</p>
+                    <p className={styles.amount}>$0.00</p>
+                </div>
+            </div>
+            <div className={styles.resetBtn}>
+            <button className={styles.btn} >RESET</button>
+            </div>
         </div>
     )
 }
